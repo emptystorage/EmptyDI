4fec4c9f0554394b0d64736d7a1146e1859602ba
Vladimir <vladimir.empty@gmail.com>
2024-01-23 19:38:30 +0300

    Project Context remove test code

--- Code/Context/ProjectContext.cs
+++ Code/Context/ProjectContext.cs
@@ -1,20 +1,6 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-
-namespace EmptyDI.Code.Context
+﻿namespace EmptyDI.Code.Context
 {
     public sealed class ProjectContext : BaseContext
     {
-        private int _sceneIndex;
-
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                _sceneIndex = (_sceneIndex == 0) ? 1 : 0;
-
-                SceneManager.LoadScene(_sceneIndex);
-            }
-        }
     }
 }
